2bb0b36ac2753e2e21b84656af0df01f1ad8f900
Samuel Čavoj <samuel@cavoj.net>
2020-08-18 16:59:48 +0200

    Add server-side config with requireOP option

---
+++ src/main/java/net/cavoj/servertick/ModConfig.java
@@ -0,0 +1,9 @@
+package net.cavoj.servertick;
+
+import me.sargunvohra.mcmods.autoconfig1u.ConfigData;
+import me.sargunvohra.mcmods.autoconfig1u.annotation.Config;
+
+@Config(name = "servertick")
+public class ModConfig implements ConfigData {
+    boolean requireOP = true;
+}
--- src/main/java/net/cavoj/servertick/ServerTick.java
+++ src/main/java/net/cavoj/servertick/ServerTick.java
@@ -1,24 +1,37 @@
 package net.cavoj.servertick;
 
+import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
+import me.sargunvohra.mcmods.autoconfig1u.ConfigHolder;
+import me.sargunvohra.mcmods.autoconfig1u.serializer.Toml4jConfigSerializer;
 import net.cavoj.servertick.extensions.MinecraftServerWithST;
+import net.fabricmc.api.EnvType;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.network.PacketContext;
 import net.fabricmc.fabric.api.network.ServerSidePacketRegistry;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.PacketByteBuf;
 
 public class ServerTick implements ModInitializer {
+    private ConfigHolder<ModConfig> configHolder;
+
     @Override
     public void onInitialize() {
         ServerSidePacketRegistry.INSTANCE.register(NetworkC2S.PACKET_ENABLED, this::processTogglePacket);
         ServerTickEvents.END_SERVER_TICK.register((minecraftServer -> {
             ((MinecraftServerWithST)minecraftServer).tickST();
         }));
+        if (FabricLoader.getInstance().getEnvironmentType() == EnvType.SERVER) {
+            AutoConfig.register(ModConfig.class, Toml4jConfigSerializer::new);
+            this.configHolder = AutoConfig.getConfigHolder(ModConfig.class);
+        }
     }
 
     private boolean checkPlayerPrivilege(PlayerEntity player) {
-        return player.hasPermissionLevel(4) || !player.getServer().isDedicated();
+        return (player.getServer() != null && !player.getServer().isDedicated()) ||
+               (this.configHolder != null && !this.configHolder.getConfig().requireOP) ||
+               player.hasPermissionLevel(4);
     }
 
     private void processTogglePacket(PacketContext ctx, PacketByteBuf data) {
