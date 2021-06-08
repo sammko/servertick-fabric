984533f613f9d81cd66d0be11e0940d4e508634c
Samuel Čavoj <samuel@cavoj.net>
2021-06-08 19:22:16 +0200

    Update to 1.17 and remove config support for now

--- src/main/java/net/cavoj/servertick/ModConfig.java
+++
@@ -1,9 +0,0 @@
-package net.cavoj.servertick;
-
-import me.sargunvohra.mcmods.autoconfig1u.ConfigData;
-import me.sargunvohra.mcmods.autoconfig1u.annotation.Config;
-
-@Config(name = "servertick")
-public class ModConfig implements ConfigData {
-    boolean requireOP = true;
-}
--- src/main/java/net/cavoj/servertick/ServerTick.java
+++ src/main/java/net/cavoj/servertick/ServerTick.java
@@ -1,8 +1,5 @@
 package net.cavoj.servertick;
 
-import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
-import me.sargunvohra.mcmods.autoconfig1u.ConfigHolder;
-import me.sargunvohra.mcmods.autoconfig1u.serializer.Toml4jConfigSerializer;
 import net.cavoj.servertick.extensions.MinecraftServerWithST;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.ModInitializer;
@@ -17,8 +14,6 @@ import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 
 public class ServerTick implements ModInitializer {
-    private ConfigHolder<ModConfig> configHolder;
-
     @Override
     public void onInitialize() {
         ServerPlayNetworking.registerGlobalReceiver(NetworkC2S.PACKET_ENABLED, this::processTogglePacket);
@@ -26,14 +21,13 @@ public class ServerTick implements ModInitializer {
             ((MinecraftServerWithST)minecraftServer).tickST();
         }));
         if (FabricLoader.getInstance().getEnvironmentType() == EnvType.SERVER) {
-            AutoConfig.register(ModConfig.class, Toml4jConfigSerializer::new);
-            this.configHolder = AutoConfig.getConfigHolder(ModConfig.class);
+            // TODO Load server config
         }
     }
 
     private boolean checkPlayerPrivilege(PlayerEntity player) {
+        // TODO check server config
         return (player.getServer() != null && !player.getServer().isDedicated()) ||
-               (this.configHolder != null && !this.configHolder.getConfig().requireOP) ||
                player.hasPermissionLevel(4);
     }
 
