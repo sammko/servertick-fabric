cea9033b76b1f23eb55f3204178f9fe571ec5368
Filip Š <filip.stamcar@hotmail.com>
2023-06-08 17:03:30 +0200

    Update to 1.20

--- src/main/java/net/cavoj/servertick/mixin/client/DebugHudMixin.java
+++ src/main/java/net/cavoj/servertick/mixin/client/DebugHudMixin.java
@@ -2,8 +2,8 @@ package net.cavoj.servertick.mixin.client;
 
 import net.cavoj.servertick.ServerTickClient;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.DebugHud;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.MetricsData;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,18 +14,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(DebugHud.class)
 public abstract class DebugHudMixin {
-    @Shadow protected abstract void drawMetricsData(MatrixStack matrixStack, MetricsData metricsData, int i, int j, boolean bl);
+    @Shadow protected abstract void drawMetricsData(DrawContext context, MetricsData metricsData, int x, int width, boolean showFps);
 
     @Shadow @Final private MinecraftClient client;
 
     @Inject(method = "render", at = @At("HEAD"))
-    public void render(MatrixStack matrices, CallbackInfo ci) {
+    public void render(DrawContext context, CallbackInfo ci) {
         if (this.client.options.debugTpsEnabled && this.client.getServer() == null) {
             MetricsData metrics = ServerTickClient.getInstance().getMetricsData();
             if (metrics == null) return;
 
-            int i = this.client.getWindow().getScaledWidth();
-            this.drawMetricsData(matrices, metrics, i - Math.min(i / 2, 240), i / 2, false);
+            context.draw(() -> {
+                int i = this.client.getWindow().getScaledWidth();
+                this.drawMetricsData(context, metrics, i - Math.min(i / 2, 240), i / 2, false);
+            });
         }
     }
 }
